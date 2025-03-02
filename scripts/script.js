5e9c63f5fb9f5546897e048da00da716fd265df7
4nik <shak.iu0@gmail.com>
2025-03-02 19:55:13 +0600

    Add random color generator

--- scripts/script.js
+++ scripts/script.js
@@ -189,3 +189,12 @@ document.getElementById("btn-task-6")
             alert("congrates!!! You have completed all the current tasks");
         }
     })
+
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
